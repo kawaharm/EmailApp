c15de7c3bd073f497bf3ba96b60d9afe9b0df728
kawaharm <mjbk295@gmail.com>
2022-04-13 15:15:50 -0700

    first commit

---
+++ src/emailApp/Email.java
@@ -0,0 +1,32 @@
+package emailApp;
+
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String dept;
+	private int mailboxCapacity;
+	private String altEmail;
+	
+	// Constructor to receive first, last name
+	public Email(String firstName, String lastName){
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+	
+	// Ask for the department
+	private String setDepartment() {
+		System.out.print("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n4 for none");
+	}
+	
+	// Generate a random password
+	
+	// Set the mailbox capacity
+	
+	// Set the alternate email
+	
+	// Change the password
+	
+	 
+}
